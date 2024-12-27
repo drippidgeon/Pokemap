e871654609abd7643c86f3101a4e4e7daebe1f57
Lukas <kubicki.lukas105@gmail.com>
2024-12-27 15:38:34 +0100

    Added React Base Pages

---
+++ React/pokemapcode/src/Page2.js
@@ -0,0 +1,8 @@
+import React from "react";
+import MapView from "./MapView";
+
+function Page2() {
+    return <MapView center={[34.0522, -118.2437]} zoom={12} />; // Example Los Angeles
+}
+
+export default Page2;
