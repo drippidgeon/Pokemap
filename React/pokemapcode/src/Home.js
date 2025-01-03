a2b7b8cc5cea1cf373b73c6008a4f99305b378e9
CreepyNiki <nlh352@gmail.com>
2025-01-03 14:40:31 +0100

    added all WebGIS Websites

--- React/pokemapcode/src/Home.js
+++ React/pokemapcode/src/Home.js
@@ -1,12 +1,26 @@
-import React from "react";
-import "leaflet/dist/leaflet.css";
+import React from 'react';
 
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to the Home Page</h1>
+      <div>
+        <form action="Kanto_base.html">
+          <input type="submit" value="Kanto" />
+        </form>
+      </div>
+      <div>
+        <form action="Johto_base.html">
+          <input type="submit" value="Johto" />
+        </form>
+      </div>
+      <div>
+        <form action="Einall_base.html">
+          <input type="submit" value="Einall" />
+        </form>
+      </div>
+    </div>
 
-
-
-
-function Home() {
-    
-}
-
+  );
+};
 export default Home;
