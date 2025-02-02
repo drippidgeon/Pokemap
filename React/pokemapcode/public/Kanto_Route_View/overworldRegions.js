83cfef9232b4c564b166f1ff0a575e1629534ce5
David-Floy <pc.tischer@gmail.com>
2025-02-02 21:16:19 +0100

    feat(kanto): Add region tileset and clickable routes
    - Added Kanto region tileset for map integration
    - Implemented clickable routes

---
+++ React/pokemapcode/public/Kanto_Route_View/overworldRegions.js
@@ -0,0 +1,414 @@
+var overworldRegions = {
+    "type": "FeatureCollection",
+    "features": [  {
+        "type": "Feature",
+        "properties": {
+            "name": "Fuchsania City",
+            "world": "Overworld",
+            "Pokémon": {}
+        },
+        "geometry": {
+            "type": "Polygon",
+            "coordinates": [[getCordfromLoc(-354.8125, 244.4375), getCordfromLoc(-394.8125, 244.4375), getCordfromLoc(-394.8125, 292.4375), getCordfromLoc(-354.8125, 292.4375), getCordfromLoc(-354.8125, 244.4375)]]
+        }
+    },  {
+        "type": "Feature",
+        "properties": {
+            "name": "Route 18",
+            "world": "Overworld",
+            "Pokémon": {}
+        },
+        "geometry": {
+            "type": "Polygon",
+            "coordinates": [[getCordfromLoc(-364.8125, 186.5), getCordfromLoc(-384.8125, 186.5), getCordfromLoc(-384.8125, 244.4375), getCordfromLoc(-364.8125, 244.4375), getCordfromLoc(-364.8125, 186.5)]]
+        }
+    },  {
+        "type": "Feature",
+        "properties": {
+            "name": "Route 17",
+            "world": "Overworld",
+            "Pokémon": {}
+        },
+        "geometry": {
+            "type": "Polygon",
+            "coordinates": [[getCordfromLoc(-204.8125, 186.5), getCordfromLoc(-364.8125, 186.5), getCordfromLoc(-364.8125, 210.5), getCordfromLoc(-204.8125, 210.5), getCordfromLoc(-204.8125, 186.5)]]
+        }
+    },{
+        "type": "Feature",
+        "properties": {
+            "name": "Route 16",
+            "world": "Overworld",
+            "Pokémon": {}
+        },
+        "geometry": {
+            "type": "Polygon",
+            "coordinates": [[getCordfromLoc(-184.8125, 186.5), getCordfromLoc(-204.8125, 186.5), getCordfromLoc(-204.8125, 234.5), getCordfromLoc(-184.8125, 234.5), getCordfromLoc(-184.8125, 186.5)]]
+        }
+    },  {
+        "type": "Feature",
+        "properties": {
+            "name": "Prismania City",
+            "world": "Overworld",
+            "Pokémon": {}
+        },
+        "geometry": {
+            "type": "Polygon",
+            "coordinates": [[getCordfromLoc(-174.8125, 234.5), getCordfromLoc(-214.8125, 234.5), getCordfromLoc(-214.8125, 294.5), getCordfromLoc(-174.8125, 294.5), getCordfromLoc(-174.8125, 234.5)]]
+        }
+    },  {
+        "type": "Feature",
+        "properties": {
+            "name": "Route 7",
+            "world": "Overworld",
+            "Pokémon": {}
+        },
+        "geometry": {
+            "type": "Polygon",
+            "coordinates": [[getCordfromLoc(-184.8125, 294.5625), getCordfromLoc(-204.875, 294.5625), getCordfromLoc(-204.875, 316.5625), getCordfromLoc(-184.8125, 316.5625), getCordfromLoc(-184.8125, 294.5625)]]
+        }
+    },  {
+        "type": "Feature",
+        "properties": {
+            "name": "Saffronia City",
+            "world": "Overworld",
+            "Pokémon": {}
+        },
+        "geometry": {
+            "type": "Polygon",
+            "coordinates": [[getCordfromLoc(-170.0625, 316.5), getCordfromLoc(-215.375, 316.5), getCordfromLoc(-215.375, 366.4375), getCordfromLoc(-170.0625, 366.4375), getCordfromLoc(-170.0625, 316.5)]]
+        }
+    },  {
+        "type": "Feature",
+        "properties": {
+            "name": "Route 6",
+            "world": "Overworld",
+            "Pokémon": {}
+        },
+        "geometry": {
+            "type": "Polygon",
+            "coordinates": [[getCordfromLoc(-215.3125, 328.5), getCordfromLoc(-254.8125, 328.5), getCordfromLoc(-254.8125, 352.5), getCordfromLoc(-215.3125, 352.5), getCordfromLoc(-215.3125, 328.5)]]
+        }
+    },  {
+        "type": "Feature",
+        "properties": {
+            "name": "Orania City",
+            "world": "Overworld",
+            "Pokémon": {}
+        },
+        "geometry": {
+            "type": "Polygon",
+            "coordinates": [[getCordfromLoc(-254.8125, 316.5), getCordfromLoc(-293.8125, 316.5), getCordfromLoc(-293.8125, 364.5), getCordfromLoc(-254.8125, 364.5), getCordfromLoc(-254.8125, 316.5)]]
+        }
+    },  {
+        "type": "Feature",
+        "properties": {
+            "name": "MS Anne",
+            "world": "Overworld",
+            "Pokémon": {}
+        },
+        "geometry": {
+            "type": "Polygon",
+            "coordinates": [[getCordfromLoc(-293.8125, 307.5), getCordfromLoc(-316.1875, 307.5), getCordfromLoc(-316.1875, 377.5), getCordfromLoc(-293.8125, 377.5), getCordfromLoc(-293.8125, 307.5)]]
+        }
+    },  {
+        "type": "Feature",
+        "properties": {
+            "name": "Route 11",
+            "world": "Overworld",
+            "Pokémon": {}
+        },
+        "geometry": {
+            "type": "Polygon",
+            "coordinates": [[getCordfromLoc(-264.8125, 364.5), getCordfromLoc(-284.8125, 364.5), getCordfromLoc(-284.8125, 436.4375), getCordfromLoc(-264.8125, 436.4375), getCordfromLoc(-264.8125, 364.5)]]
+        }
+    },  {
+        "type": "Feature",
+        "properties": {
+            "name": "Route 12",
+            "world": "Overworld",
+            "Pokémon": {}
+        },
+        "geometry": {
+            "type": "Polygon",
+            "coordinates": [[getCordfromLoc(-204.8125, 436.4375), getCordfromLoc(-324.8125, 436.4375), getCordfromLoc(-324.8125, 460.4375), getCordfromLoc(-204.8125, 460.4375), getCordfromLoc(-204.8125, 436.4375)]]
+        }
+    },  {
+        "type": "Feature",
+        "properties": {
+            "name": "Lavandia",
+            "world": "Overworld",
+            "Pokémon": {}
+        },
+        "geometry": {
+            "type": "Polygon",
+            "coordinates": [[getCordfromLoc(-177.125, 436.5), getCordfromLoc(-204.8125, 436.5), getCordfromLoc(-204.8125, 460.5), getCordfromLoc(-177.125, 460.5), getCordfromLoc(-177.125, 436.5)]]
+        }
+    },  {
+        "type": "Feature",
+        "properties": {
+            "name": "Route 8",
+            "world": "Overworld",
+            "Pokémon": {}
+        },
+        "geometry": {
+            "type": "Polygon",
+            "coordinates": [[getCordfromLoc(-184.8125, 366.375), getCordfromLoc(-204.8125, 366.375), getCordfromLoc(-204.8125, 436.4375), getCordfromLoc(-184.8125, 436.4375), getCordfromLoc(-184.8125, 366.375)]]
+        }
+    },  {
+        "type": "Feature",
+        "properties": {
+            "name": "Route 10",
+            "world": "Overworld",
+            "Pokémon": {}
+        },
+        "geometry": {
+            "type": "Polygon",
+            "coordinates": [[getCordfromLoc(-105.125, 436.4375), getCordfromLoc(-177.1875, 436.4375), getCordfromLoc(-177.1875, 460.5), getCordfromLoc(-105.125, 460.5), getCordfromLoc(-105.125, 436.4375)]]
+        }
+    },  {
+        "type": "Feature",
+        "properties": {
+            "name": "Route 9",
+            "world": "Overworld",
+            "Pokémon": {}
+        },
+        "geometry": {
+            "type": "Polygon",
+            "coordinates": [[getCordfromLoc(-105.125, 364.5), getCordfromLoc(-125.125, 364.5), getCordfromLoc(-125.125, 436.4375), getCordfromLoc(-105.125, 436.4375), getCordfromLoc(-105.125, 364.5)]]
+        }
+    },  {
+        "type": "Feature",
+        "properties": {
+            "name": "Azuria City",
+            "world": "Overworld",
+            "Pokémon": {}
+        },
+        "geometry": {
+            "type": "Polygon",
+            "coordinates": [[getCordfromLoc(-95.125, 316.5), getCordfromLoc(-131.25, 316.5), getCordfromLoc(-131.25, 364.5), getCordfromLoc(-95.125, 364.5), getCordfromLoc(-95.125, 316.5)]]
+        }
+    },  {
+        "type": "Feature",
+        "properties": {
+            "name": "Route 5",
+            "world": "Overworld",
+            "Pokémon": {}
+        },
+        "geometry": {
+            "type": "Polygon",
+            "coordinates": [[getCordfromLoc(-131.25, 316.5), getCordfromLoc(-170.0625, 316.5), getCordfromLoc(-170.0625, 364.4375), getCordfromLoc(-131.25, 364.4375), getCordfromLoc(-131.25, 316.5)]]
+        }
+    },  {
+        "type": "Feature",
+        "properties": {
+            "name": "Route 24",
+            "world": "Overworld",
+            "Pokémon": {}
+        },
+        "geometry": {
+            "type": "Polygon",
+            "coordinates": [[getCordfromLoc(-55.125, 328.5), getCordfromLoc(-95.125, 328.5), getCordfromLoc(-95.125, 352.5), getCordfromLoc(-55.125, 352.5), getCordfromLoc(-55.125, 328.5)]]
+        }
+    },  {
+        "type": "Feature",
+        "properties": {
+            "name": "Route 25",
+            "world": "Overworld",
+            "Pokémon": {}
+        },
+        "geometry": {
+            "type": "Polygon",
+            "coordinates": [[getCordfromLoc(-55.125, 352.5), getCordfromLoc(-75.125, 352.5), getCordfromLoc(-75.125, 424.5), getCordfromLoc(-55.125, 424.5), getCordfromLoc(-55.125, 352.5)]]
+        }
+    },  {
+        "type": "Feature",
+        "properties": {
+            "name": "Route 4",
+            "world": "Overworld",
+            "Pokémon": {}
+        },
+        "geometry": {
+            "type": "Polygon",
+            "coordinates": [[getCordfromLoc(-105.125, 208.5), getCordfromLoc(-125.125, 208.5), getCordfromLoc(-125.125, 316.5625), getCordfromLoc(-105.125, 316.5625), getCordfromLoc(-105.125, 208.5)]]
+        }
+    },  {
+        "type": "Feature",
+        "properties": {
+            "name": "Route 3",
+            "world": "Overworld",
+            "Pokémon": {}
+        },
+        "geometry": {
+            "type": "Polygon",
+            "coordinates": [[getCordfromLoc(-125.125, 148.5), getCordfromLoc(-145.125, 148.5), getCordfromLoc(-145.125, 235.5), getCordfromLoc(-125.125, 235.5), getCordfromLoc(-125.125, 148.5)]]
+        }
+    },  {
+        "type": "Feature",
+        "properties": {
+            "name": "Marmoria City",
+            "world": "Overworld",
+            "Pokémon": {}
+        },
+        "geometry": {
+            "type": "Polygon",
+            "coordinates": [[getCordfromLoc(-114.125, 100.5), getCordfromLoc(-151.25, 100.5), getCordfromLoc(-151.25, 148.5), getCordfromLoc(-114.125, 148.5), getCordfromLoc(-114.125, 100.5)]]
+        }
+    },  {
+        "type": "Feature",
+        "properties": {
+            "name": "Route 2",
+            "world": "Overworld",
+            "Pokémon": {}
+        },
+        "geometry": {
+            "type": "Polygon",
+            "coordinates": [[getCordfromLoc(-151.3125, 112.5), getCordfromLoc(-231.1875, 112.5), getCordfromLoc(-231.1875, 136.5), getCordfromLoc(-151.3125, 136.5), getCordfromLoc(-151.3125, 112.5)]]
+        }
+    },  {
+        "type": "Feature",
+        "properties": {
+            "name": "Vertania City",
+            "world": "Overworld",
+            "Pokémon": {}
+        },
+        "geometry": {
+            "type": "Polygon",
+            "coordinates": [[getCordfromLoc(-231.1875, 100.5), getCordfromLoc(-271.1875, 100.5), getCordfromLoc(-271.1875, 148.5), getCordfromLoc(-231.1875, 148.5), getCordfromLoc(-231.1875, 100.5)]]
+        }
+    },  {
+        "type": "Feature",
+        "properties": {
+            "name": "Route 1",
+            "world": "Overworld",
+            "Pokémon": {}
+        },
+        "geometry": {
+            "type": "Polygon",
+            "coordinates": [[getCordfromLoc(-271.1875, 112.5), getCordfromLoc(-311.375, 112.5), getCordfromLoc(-311.375, 136.5), getCordfromLoc(-271.1875, 136.5), getCordfromLoc(-271.1875, 112.5)]]
+        }
+    },  {
+        "type": "Feature",
+        "properties": {
+            "name": "Alabastia",
+            "world": "Overworld",
+            "Pokémon": {}
+        },
+        "geometry": {
+            "type": "Polygon",
+            "coordinates": [[getCordfromLoc(-311.3125, 112.5), getCordfromLoc(-332.5625, 112.5), getCordfromLoc(-332.5625, 136.5), getCordfromLoc(-311.3125, 136.5), getCordfromLoc(-311.3125, 112.5)]]
+        }
+    },  {
+        "type": "Feature",
+        "properties": {
+            "name": "Route 21",
+            "world": "Overworld",
+            "Pokémon": {}
+        },
+        "geometry": {
+            "type": "Polygon",
+            "coordinates": [[getCordfromLoc(-332.5, 112.5), getCordfromLoc(-431.125, 112.5), getCordfromLoc(-431.125, 136.5), getCordfromLoc(-332.5, 136.5), getCordfromLoc(-332.5, 112.5)]]
+        }
+    },  {
+        "type": "Feature",
+        "properties": {
+            "name": "Zinnoberinsel",
+            "world": "Overworld",
+            "Pokémon": {}
+        },
+        "geometry": {
+            "type": "Polygon",
+            "coordinates": [[getCordfromLoc(-431.0625, 112.5), getCordfromLoc(-451.125, 112.5), getCordfromLoc(-451.125, 136.5), getCordfromLoc(-431.0625, 136.5), getCordfromLoc(-431.0625, 112.5)]]
+        }
+    },  {
+        "type": "Feature",
+        "properties": {
+            "name": "Route 20",
+            "world": "Overworld",
+            "Pokémon": {}
+        },
+        "geometry": {
+            "type": "Polygon",
+            "coordinates": [[getCordfromLoc(-431.125, 136.4375), getCordfromLoc(-451.125, 136.4375), getCordfromLoc(-451.125, 256.4375), getCordfromLoc(-431.125, 256.4375), getCordfromLoc(-431.125, 136.4375)]]
+        }
+    },  {
+        "type": "Feature",
+        "properties": {
+            "name": "Route 19",
+            "world": "Overworld",
+            "Pokémon": {}
+        },
+        "geometry": {
+            "type": "Polygon",
+            "coordinates": [[getCordfromLoc(-394.8125, 256.4375), getCordfromLoc(-454.875, 256.4375), getCordfromLoc(-454.875, 280.4375), getCordfromLoc(-394.8125, 280.4375), getCordfromLoc(-394.8125, 256.4375)]]
+        }
+    },  {
+        "type": "Feature",
+        "properties": {
+            "name": "Route 15",
+            "world": "Overworld",
+            "Pokémon": {}
+        },
+        "geometry": {
+            "type": "Polygon",
+            "coordinates": [[getCordfromLoc(-364.875, 292.4375), getCordfromLoc(-384.8125, 292.4375), getCordfromLoc(-384.8125, 364.4375), getCordfromLoc(-364.875, 364.4375), getCordfromLoc(-364.875, 292.4375)]]
+        }
+    },  {
+        "type": "Feature",
+        "properties": {
+            "name": "Route 14",
+            "world": "Overworld",
+            "Pokémon": {}
+        },
+        "geometry": {
+            "type": "Polygon",
+            "coordinates": [[getCordfromLoc(-324.75, 364.375), getCordfromLoc(-384.8125, 364.375), getCordfromLoc(-384.8125, 388.4375), getCordfromLoc(-324.75, 388.4375), getCordfromLoc(-324.75, 364.375)]]
+        }
+    },  {
+        "type": "Feature",
+        "properties": {
+            "name": "Route 13",
+            "world": "Overworld",
+            "Pokémon": {}
+        },
+        "geometry": {
+            "type": "Polygon",
+            "coordinates": [[getCordfromLoc(-324.875, 388.4375), getCordfromLoc(-344.8125, 388.4375), getCordfromLoc(-344.8125, 460.4375), getCordfromLoc(-324.875, 460.4375), getCordfromLoc(-324.875, 388.4375)]]
+        }
+    },  {
+        "type": "Feature",
+        "properties": {
+            "name": "Route 22",
+            "world": "Overworld",
+            "Pokémon": {}
+        },
+        "geometry": {
+            "type": "Polygon",
+            "coordinates": [[getCordfromLoc(-241.1875, 49.5), getCordfromLoc(-265.6875, 49.5), getCordfromLoc(-265.6875, 100.5), getCordfromLoc(-241.1875, 100.5), getCordfromLoc(-241.1875, 49.5)]]
+        }
+    },  {
+        "type": "Feature",
+        "properties": {
+            "name": "Route 23",
+            "world": "Overworld",
+            "Pokémon": {}
+        },
+        "geometry": {
+            "type": "Polygon",
+            "coordinates": [[getCordfromLoc(-79.25, 52.5625), getCordfromLoc(-241.1875, 52.5625), getCordfromLoc(-241.1875, 76.5), getCordfromLoc(-79.25, 76.5), getCordfromLoc(-79.25, 52.5625)]]
+        }
+    },  {
+        "type": "Feature",
+        "properties": {
+            "name": "Indigo-Plateau",
+            "world": "Overworld",
+            "Pokémon": {}
+        },
+        "geometry": {
+            "type": "Polygon",
+            "coordinates": [[getCordfromLoc(-61.1875, 52.5625), getCordfromLoc(-79.25, 52.5625), getCordfromLoc(-79.25, 76.5), getCordfromLoc(-61.1875, 76.5), getCordfromLoc(-61.1875, 52.5625)]]
+        }
+    },
+
+    ]
+}
+
