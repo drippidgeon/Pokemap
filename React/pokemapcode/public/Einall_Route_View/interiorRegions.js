4395ba8a8d9f2be126949a0f432e4d7897b782b0
Lukas <kubicki.lukas105@gmail.com>
2025-03-07 00:15:14 +0100

    Added tilesets and Code for Interior Regiosn
    Code is not fully functional, some maps dont load properly onclick

---
+++ React/pokemapcode/public/Einall_Route_View/interiorRegions.js
@@ -0,0 +1,346 @@
+
+var alterpalastRegions = {
+    'type': 'FeatureCollection',
+    'features': [
+      {
+        'type': 'Feature',
+        'properties': {
+          'name': 'Alter palast',
+          'world': 'Overworld',
+          'Pokémon': {},
+        },
+        'geometry': {
+          'type': 'Polygon',
+          'coordinates': [],
+        }
+      }
+    ]
+};
+
+var beschwörungshöhleRegions = {
+    'type': 'FeatureCollection',
+    'features': [
+      {
+        'type': 'Feature',
+        'properties': {
+          'name': 'Beschwörungshöhle',
+          'world': 'Overworld',
+          'Pokémon': {},
+        },
+        'geometry': {
+          'type': 'Polygon',
+          'coordinates': [],
+        }
+      }
+    ]
+};
+
+var drachenstiegeRegions = {
+    'type': 'FeatureCollection',
+    'features': [
+      {
+        'type': 'Feature',
+        'properties': {
+          'name': 'Drachenstiege',
+          'world': 'Overworld',
+          'Pokémon': {},
+        },
+        'geometry': {
+          'type': 'Polygon',
+          'coordinates': [],
+        }
+      }
+    ]
+};
+
+var elektrolithöhleRegions = {
+    'type': 'FeatureCollection',
+    'features': [
+      {
+        'type': 'Feature',
+        'properties': {
+          'name': 'Elektrolithöhle',
+          'world': 'Overworld',
+          'Pokémon': {},
+        },
+        'geometry': {
+          'type': 'Polygon',
+          'coordinates': [],
+        }
+      }
+    ]
+};
+
+var feldderbesinnungRegions = {
+    'type': 'FeatureCollection',
+    'features': [
+      {
+        'type': 'Feature',
+        'properties': {
+          'name': 'Feld der Besinnung',
+          'world': 'Overworld',
+          'Pokémon': {},
+        },
+        'geometry': {
+          'type': 'Polygon',
+          'coordinates': [],
+        }
+      }
+    ]
+};
+
+var grundwassersenkeRegions = {
+    'type': 'FeatureCollection',
+    'features': [
+      {
+        'type': 'Feature',
+        'properties': {
+          'name': 'Grundwassersenke',
+          'world': 'Overworld',
+          'Pokémon': {},
+        },
+        'geometry': {
+          'type': 'Polygon',
+          'coordinates': [],
+        }
+      }
+    ]
+};
+
+
+var haindertäuschungRegions = {
+    'type': 'FeatureCollection',
+    'features': [
+      {
+        'type': 'Feature',
+        'properties': {
+          'name': 'Hain der Täuschung',
+          'world': 'Overworld',
+          'Pokémon': {},
+        },
+        'geometry': {
+          'type': 'Polygon',
+          'coordinates': [],
+        }
+      }
+    ]
+};
+
+
+var janusbergRegions = {
+    'type': 'FeatureCollection',
+    'features': [
+      {
+        'type': 'Feature',
+        'properties': {
+          'name': 'Janusberg',
+          'world': 'Overworld',
+          'Pokémon': {},
+        },
+        'geometry': {
+          'type': 'Polygon',
+          'coordinates': [],
+        }
+      }
+    ]
+};
+
+var panaerohöhleRegions = {
+    'type': 'FeatureCollection',
+    'features': [
+      {
+        'type': 'Feature',
+        'properties': {
+          'name': 'Panaero-Höhle',
+          'world': 'Overworld',
+          'Pokémon': {},
+        },
+        'geometry': {
+          'type': 'Polygon',
+          'coordinates': [],
+        }
+      }
+    ]
+};
+
+var riesengrotteRegions = {
+    'type': 'FeatureCollection',
+    'features': [
+      {
+        'type': 'Feature',
+        'properties': {
+          'name': 'Riesengrotte-Höhle',
+          'world': 'Overworld',
+          'Pokémon': {},
+        },
+        'geometry': {
+          'type': 'Polygon',
+          'coordinates': [],
+        }
+      }
+    ]
+};
+
+var riesengrottetiefsterteilRegions = {
+    'type': 'FeatureCollection',
+    'features': [
+      {
+        'type': 'Feature',
+        'properties': {
+          'name': 'Riesengrotte-Tiefster-teil',
+          'world': 'Overworld',
+          'Pokémon': {},
+        },
+        'geometry': {
+          'type': 'Polygon',
+          'coordinates': [],
+        }
+      }
+    ]
+};
+
+var schreinderernteRegions = {
+    'type': 'FeatureCollection',
+    'features': [
+      {
+        'type': 'Feature',
+        'properties': {
+          'name': 'Schrein der Ernte',
+          'world': 'Overworld',
+          'Pokémon': {},
+        },
+        'geometry': {
+          'type': 'Polygon',
+          'coordinates': [],
+        }
+      }
+    ]
+};
+
+var schwurhainRegions = {
+    'type': 'FeatureCollection',
+    'features': [
+      {
+        'type': 'Feature',
+        'properties': {
+          'name': 'Schwurhain',
+          'world': 'Overworld',
+          'Pokémon': {},
+        },
+        'geometry': {
+          'type': 'Polygon',
+          'coordinates': [],
+        }
+      }
+    ]
+};
+
+var strandgrotteRegions = {
+    'type': 'FeatureCollection',
+    'features': [
+      {
+        'type': 'Feature',
+        'properties': {
+          'name': 'Strandgrotte',
+          'world': 'Overworld',
+          'Pokémon': {},
+        },
+        'geometry': {
+          'type': 'Polygon',
+          'coordinates': [],
+        }
+      }
+    ]
+};
+
+var stratoskanalisationRegions = {
+    'type': 'FeatureCollection',
+    'features': [
+      {
+        'type': 'Feature',
+        'properties': {
+          'name': 'Stratos-Kanalisation',
+          'world': 'Overworld',
+          'Pokémon': {},
+        },
+        'geometry': {
+          'type': 'Polygon',
+          'coordinates': [],
+        }
+      }
+    ]
+};
+
+var traumbrachekellerRegions = {
+    'type': 'FeatureCollection',
+    'features': [
+      {
+        'type': 'Feature',
+        'properties': {
+          'name': 'Traumbrache-Keller',
+          'world': 'Overworld',
+          'Pokémon': {},
+        },
+        'geometry': {
+          'type': 'Polygon',
+          'coordinates': [],
+        }
+      }
+    ]
+};
+
+var turnerpfadRegions = {
+    'type': 'FeatureCollection',
+    'features': [
+      {
+        'type': 'Feature',
+        'properties': {
+          'name': 'Turner-Pfad',
+          'world': 'Overworld',
+          'Pokémon': {},
+        },
+        'geometry': {
+          'type': 'Polygon',
+          'coordinates': [],
+        }
+      }
+    ]
+};
+
+
+var unterseetunnelRegions = {
+    'type': 'FeatureCollection',
+    'features': [
+      {
+        'type': 'Feature',
+        'properties': {
+          'name': 'Unterseetunnel',
+          'world': 'Overworld',
+          'Pokémon': {},
+        },
+        'geometry': {
+          'type': 'Polygon',
+          'coordinates': [],
+        }
+      }
+    ]
+};
+
+
+var unterwasserruineRegions = {
+    'type': 'FeatureCollection',
+    'features': [
+      {
+        'type': 'Feature',
+        'properties': {
+          'name': 'Unterwasserruine',
+          'world': 'Overworld',
+          'Pokémon': {},
+        },
+        'geometry': {
+          'type': 'Polygon',
+          'coordinates': [],
+        }
+      }
+    ]
+};
--- React/pokemapcode/public/Einall_Route_View/maps.js
+++ React/pokemapcode/public/Einall_Route_View/maps.js
@@ -8,6 +8,178 @@ var maps = {
         "vector": overworldRegions,
         "nowrap": true
     },
-   
+
+"Alter Palast": { 
+        "shortname": "alter-palast", 
+        "zoom": 2, 
+        "minZoom": 2,
+        "maxZoom": 4,
+        "bounds": [-128, 128],
+        "vector": alterpalastRegions,
+        "nowrap": true 
+    },
+    "Beschwörungshöhle": { 
+        "shortname": "beschwoerungshoehle", 
+        "zoom": 2, 
+        "minZoom": 2,
+        "maxZoom": 4,
+        "bounds": [-128, 128],
+        "vector": beschwörungshöhleRegions,
+        "nowrap": true 
+    },
+    "Drachenstiege": { 
+        "shortname": "drachenstiege", 
+        "zoom": 2, 
+        "minZoom": 2,
+        "maxZoom": 4,
+        "bounds": [-128, 128],
+        "vector": drachenstiegeRegions,
+        "nowrap": true 
+    },
+    "Elektrolithöhle": { 
+        "shortname": "elektrolithoehle", 
+        "zoom": 2, 
+        "minZoom": 2,
+        "maxZoom": 4,
+        "bounds": [-128, 128],
+        "vector": elektrolithöhleRegions,
+        "nowrap": true 
+    },
+    "Feld der Besinnung": { 
+        "shortname": "feld-der-besinnung", 
+        "zoom": 2, 
+        "minZoom": 2,
+        "maxZoom": 4,
+        "bounds": [-128, 128],
+        "vector": feldderbesinnungRegions,
+        "nowrap": true 
+    },
+    "Grundwassersenke": { 
+        "shortname": "grundwassersenke", 
+        "zoom": 2, 
+        "minZoom": 2,
+        "maxZoom": 4,
+        "bounds": [-128, 128],
+        "vector": grundwassersenkeRegions,
+        "nowrap": true 
+    },
+    "Hain der Täuschung": { 
+        "shortname": "hain-der-taeuschung", 
+        "zoom": 2, 
+        "minZoom": 2,
+        "maxZoom": 4,
+        "bounds": [-128, 128],
+        "vector": haindertäuschungRegions,
+        "nowrap": true 
+    },
+    "Janusberg": { 
+        "shortname": "janusberg", 
+        "zoom": 2, 
+        "minZoom": 2,
+        "maxZoom": 4,
+        "bounds": [-128, 128],
+        "vector": janusbergRegions,
+        "nowrap": true 
+    },
+    "Panaero-Höhle": { 
+        "shortname": "panaero-hoehle", 
+        "zoom": 2, 
+        "minZoom": 2,
+        "maxZoom": 4,
+        "bounds": [-128, 128],
+        "vector": panaerohöhleRegions,
+        "nowrap": true 
+    },
+    "Riesengrotte-Höhle": { 
+        "shortname": "riesengrotte-hoehle", 
+        "zoom": 2, 
+        "minZoom": 2,
+        "maxZoom": 4,
+        "bounds": [-128, 128],
+        "vector": riesengrotteRegions,
+        "nowrap": true 
+    },
+    "Riesengrotte-Tiefster-Teil": { 
+        "shortname": "riesengrotte-tiefster-teil", 
+        "zoom": 2, 
+        "minZoom": 2,
+        "maxZoom": 4,
+        "bounds": [-128, 128],
+        "vector": riesengrottetiefsterteilRegions,
+        "nowrap": true 
+    },
+    "Schrein der Ernte": { 
+        "shortname": "schrein-der-ernte", 
+        "zoom": 2, 
+        "minZoom": 2,
+        "maxZoom": 4,
+        "bounds": [-128, 128],
+        "vector": schreinderernteRegions,
+        "nowrap": true 
+    },
+    "Schwurhain": { 
+        "shortname": "schwurhain", 
+        "zoom": 2, 
+        "minZoom": 2,
+        "maxZoom": 4,
+        "bounds": [-128, 128],
+        "vector": schwurhainRegions,
+        "nowrap": true 
+    },
+    "Strandgrotte": { 
+        "shortname": "strandgrotte", 
+        "zoom": 2, 
+        "minZoom": 2,
+        "maxZoom": 4,
+        "bounds": [-128, 128],
+        "vector": strandgrotteRegions,
+        "nowrap": true 
+    },
+    "Stratos-Kanalisation": { 
+        "shortname": "stratos-kanalisation", 
+        "zoom": 2, 
+        "minZoom": 2,
+        "maxZoom": 4,
+        "bounds": [-128, 128],
+        "vector": stratoskanalisationRegions,
+        "nowrap": true 
+    },
+    "Traumbrache-Keller": { 
+        "shortname": "traumbrache-keller", 
+        "zoom": 2, 
+        "minZoom": 2,
+        "maxZoom": 4,
+        "bounds": [-128, 128],
+        "vector": traumbrachekellerRegions,
+        "nowrap": true 
+    },
+    "Turner-Pfad": { 
+        "shortname": "turner-pfad", 
+        "zoom": 2, 
+        "minZoom": 2,
+        "maxZoom": 4,
+        "bounds": [-128, 128],
+        "vector": turnerpfadRegions,
+        "nowrap": true 
+    },
+    "Unterseetunnel": { 
+        "shortname": "unterseetunnel", 
+        "zoom": 2, 
+        "minZoom": 2,
+        "maxZoom": 4,
+        "bounds": [-128, 128],
+        "vector": unterseetunnelRegions,
+        "nowrap": true 
+    },
+    "Unterwasserruine": { 
+        "shortname": "unterwasserruine", 
+        "zoom": 2, 
+        "minZoom": 2,
+        "maxZoom": 4,
+        "bounds": [-128, 128],
+        "vector": unterwasserruineRegions,
+        "nowrap": true 
+    }
+
 }
 
