a86101b717193a807fbd4e09c8929b7097366eff
Lukas <kubicki.lukas105@gmail.com>
2025-03-06 11:52:33 +0100

    added einall entrance markers and tilesets *not complete*

--- React/pokemapcode/public/Einall_Route_View/overworldMarkers.js
+++ React/pokemapcode/public/Einall_Route_View/overworldMarkers.js
@@ -2,3 +2,31 @@ var currentMap = "Overworld";
 // Entrances
 
 
+markerSet(-371.077, 677.370, "Feld der Besinnung", entranceIcon, currentMap);
+markerSet(-354.576, 521.395, "Stratos-Kanalisation", entranceIcon, currentMap);
+markerSet(-223.790, 527.552, "Hain der Täuschung", entranceIcon, currentMap);
+markerSet(-318.262, 212.602, "Schwurhain", entranceIcon, currentMap);
+markerSet(-341.763, 224.290, "Beschwörungshöhle", entranceIcon, currentMap);
+markerSet(-212.758, 303.324, "Turner-Pfad", entranceIcon, currentMap);
+markerSet(-198.320, 297.105, "Panaero-Höhle", entranceIcon, currentMap);
+markerSet(-187.351, 268.510, "Elektrolithöhle", entranceIcon, currentMap);
+markerSet(-174.757, 266.229, "Elektrolithöhle", entranceIcon, currentMap);
+markerSet(-146.381, 708.840, "Strandgrotte", entranceIcon, currentMap);
+markerSet(-137.037, 709.090, "Strandgrotte", entranceIcon, currentMap);
+markerSet(-136.724, 713.809, "Strandgrotte", entranceIcon, currentMap);
+markerSet(-94.441, 651.775, "Riesengrotte-Höhle", entranceIcon, currentMap);
+markerSet(-86.128, 627.462, "Riesengrotte-Höhle", entranceIcon, currentMap);
+markerSet(-395.672, 841.845, "Traumbrache-Keller", entranceIcon, currentMap);
+markerSet(-399.328, 859.409, "Traumbrache-Keller", entranceIcon, currentMap);
+markerSet(-376.734, 750.186, "Grundwassersenke", entranceIcon, currentMap);
+markerSet(-162.694, 682.277, "Unterseetunnel", entranceIcon, currentMap);
+markerSet(-107.535, 713.840, "Unterseetunnel", entranceIcon, currentMap);
+markerSet(-164.256, 666.120, "Janusberg", entranceIcon, currentMap);
+markerSet(-167.007, 647.119, "Janusberg", entranceIcon, currentMap);
+markerSet(-164.194, 650.369, "Janusberg", entranceIcon, currentMap);
+markerSet(-162.881, 639.306, "Janusberg", entranceIcon, currentMap);
+markerSet(-168.413, 719.841, "Unterwasserruine", entranceIcon, currentMap);
+markerSet(-251.979, 450.549, "Alter Palast", entranceIcon, currentMap);
+markerSet(-382.765, 859.659, "Traumbrache-Keller", entranceIcon, currentMap);
+markerSet(-65.659, 344.763, "Drachenstiege", entranceIcon, currentMap);
+markerSet(-189.289, 648.275, "Schrein der Ernte", entranceIcon, currentMap);
