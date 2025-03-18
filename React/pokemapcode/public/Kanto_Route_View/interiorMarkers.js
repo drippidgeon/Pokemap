608c93f45e9b1eab7e208188da66346ecd73634c
Lisakri3 <lisa.krings3@gmail.com>
2025-03-18 12:50:04 +0100

    translated InteriorMarkers

--- React/pokemapcode/public/Kanto_Route_View/interiorMarkers.js
+++ React/pokemapcode/public/Kanto_Route_View/interiorMarkers.js
@@ -1,117 +1,109 @@
 //test mit den interiors
 var currentMap = "Azuria-Höhle";
 markerSet(-384.88, 309.63, "Overworld", entranceIcon, currentMap);
-markerSet(-250.75, 310.38, "Full Restore", itemIcon, currentMap);
-markerSet(-307.50, 225.13, "Ultra Ball", hiddenIcon, currentMap);
-markerSet(-311.75, 206.38, "Nugget", itemIcon, currentMap);
-markerSet(-275.50, 213.75, "PP UP", itemIcon, currentMap);
-markerSet(-363.25, 221.63, "Full Restore", itemIcon, currentMap);
-markerSet(-267.25, 293.75, "Ultra Ball", itemIcon, currentMap);
-markerSet(-319.13, 277.63, "Max Elixir", itemIcon, currentMap);
-markerSet(-116.13, 305.50, "Max Revive", itemIcon, currentMap);
-markerSet(-147.88, 301.63, "Ultra Ball", itemIcon, currentMap);
+markerSet(-250.75, 310.38, "Top-Genesung", itemIcon, currentMap);
+markerSet(-307.50, 225.13, "Hyperball", hiddenIcon, currentMap);
+markerSet(-311.75, 206.38, "Goldene Nugget", itemIcon, currentMap);
+markerSet(-275.50, 213.75, "AP-Plus", itemIcon, currentMap);
+markerSet(-363.25, 221.63, "Top-Genesung", itemIcon, currentMap);
+markerSet(-267.25, 293.75, "Hyperball", itemIcon, currentMap);
+markerSet(-319.13, 277.63, "Elixier", itemIcon, currentMap);
+markerSet(-116.13, 305.50, "Top-Beleber", itemIcon, currentMap);
+markerSet(-147.88, 301.63, "Hyperball", itemIcon, currentMap);
 
 var currentMap = "Digda-Höhle";
 markerSet(-87.25, 87.25, "Overworld", entranceIcon, currentMap);
 markerSet(-391.50, 445.00, "Overworld", entranceIcon, currentMap);
 
-
 var currentMap = "Vertania-Wald";
 markerSet(-169.00, 174.00, "Overworld", entranceIcon, currentMap);
 markerSet(-380.00, 271.00, "Overworld", entranceIcon, currentMap);
-markerSet(-357.75, 265.50, "Antidote", hiddenIcon, currentMap);
-markerSet(-293.25, 172.25, "Pokeball", itemIcon, currentMap);
-markerSet(-369.00, 349.00, "Super Potion", itemIcon, currentMap);
-markerSet(-212.88, 312.25, "Antidote", itemIcon, currentMap);
-markerSet(-265.13, 236.50, "Potion", itemIcon, currentMap);
-markerSet(-214.13, 164.50, "Potion", hiddenIcon, currentMap);
-
+markerSet(-357.75, 265.50, "Gegengift", hiddenIcon, currentMap);
+markerSet(-293.25, 172.25, "Pokéball", itemIcon, currentMap);
+markerSet(-369.00, 349.00, "Supertrank", itemIcon, currentMap);
+markerSet(-212.88, 312.25, "Gegengift", itemIcon, currentMap);
+markerSet(-265.13, 236.50, "Trank", itemIcon, currentMap);
+markerSet(-214.13, 164.50, "Trank", hiddenIcon, currentMap);
 
 var currentMap = "Seeschauminseln";
 markerSet(-490.50, 204.25, "Overworld", entranceIcon, currentMap);
 markerSet(-491.63, 307.63, "Overworld", entranceIcon, currentMap);
-markerSet(-160.13, 201.25, "Nugget", hiddenIcon, currentMap);
-markerSet(-44.25, 232.13, "Water Stone", hiddenIcon, currentMap);
-markerSet(-88.00, 267.25, "Kings Rock", itemIcon, currentMap);
-markerSet(-269.63, 252.00, "Pearl", itemIcon, currentMap);
-markerSet(-381.50, 254.75, "Water Stone", itemIcon, currentMap);
-markerSet(-363.38, 271.50, "Revive", itemIcon, currentMap);
-markerSet(-431.88, 224.50, "Hyper Potion", itemIcon, currentMap);
-
+markerSet(-160.13, 201.25, "Goldene Nugget", hiddenIcon, currentMap);
+markerSet(-44.25, 232.13, "Wasserstein", hiddenIcon, currentMap);
+markerSet(-88.00, 267.25, "King-Stein", itemIcon, currentMap);
+markerSet(-269.63, 252.00, "Perle", itemIcon, currentMap);
+markerSet(-381.50, 254.75, "Wasserstein", itemIcon, currentMap);
+markerSet(-363.38, 271.50, "Beleber", itemIcon, currentMap);
+markerSet(-431.88, 224.50, "Hypertrank", itemIcon, currentMap);
 
 var currentMap = "Mondberg";
 markerSet(-117.88, 99.88, "Overworld", entranceIcon, currentMap);
 markerSet(-360.38, 346.25, "Overworld", entranceIcon, currentMap);
-markerSet(-343.00, 318.25, "TM12(Water Gun)", tmIcon, currentMap);
-markerSet(-296.38, 281.88, "Parlyz Heal", itemIcon, currentMap);
-markerSet(-382.00, 232.13, "TinyMushroom", hiddenIcon, currentMap);
-markerSet(-385.38, 173.25, "TinyMushroom", hiddenIcon, currentMap);
-markerSet(-181.13, 200.13, "Silk Scarf", itemIcon, currentMap);
-markerSet(-382.00, 175.63, "Bis Mushroom", hiddenIcon, currentMap);
-markerSet(-290.75, 448.13, "Escape Rope", itemIcon, currentMap);
-markerSet(-350.00, 444.38, "Rare Candy", itemIcon, currentMap);
-markerSet(-336.13, 379.00, "Potion", itemIcon, currentMap);
-markerSet(-253.50, 178.50, "TinyMushroom", hiddenIcon, currentMap);
-markerSet(-121.75, 234.88, "Ether", hiddenIcon, currentMap);
-markerSet(-96.88, 220.75, "TM(Assurance)", tmIcon, currentMap);
-markerSet(-215.63, 286.00, "Moon Stone", itemIcon, currentMap);
-markerSet(-293.25, 99.25, "Big Mushroom", hiddenIcon, currentMap);
-markerSet(-100.75, 175.25, "Revive", itemIcon, currentMap);
-markerSet(-141.38, 158.38, "Moon Stone", hiddenIcon, currentMap);
-markerSet(-121.25, 91.88, "Antidote", itemIcon, currentMap);
-markerSet(-252.88, 258.25, "TinyMushroom", hiddenIcon, currentMap);
-
-
+markerSet(-343.00, 318.25, "TM12(Aquaknarre)", tmIcon, currentMap);
+markerSet(-296.38, 281.88, "Para-Heiler", itemIcon, currentMap);
+markerSet(-382.00, 232.13, "Mini-Pilz", hiddenIcon, currentMap);
+markerSet(-385.38, 173.25, "Mini-Pilz", hiddenIcon, currentMap);
+markerSet(-181.13, 200.13, "Seidenschal", itemIcon, currentMap);
+markerSet(-382.00, 175.63, "Riesenpilz", hiddenIcon, currentMap);
+markerSet(-290.75, 448.13, "Fluchtseil", itemIcon, currentMap);
+markerSet(-350.00, 444.38, "Sonderbonbon", itemIcon, currentMap);
+markerSet(-336.13, 379.00, "Trank", itemIcon, currentMap);
+markerSet(-253.50, 178.50, "Mini-Pilz", hiddenIcon, currentMap);
+markerSet(-121.75, 234.88, "Äther", hiddenIcon, currentMap);
+markerSet(-96.88, 220.75, "TM(Verfolgung)", tmIcon, currentMap);
+markerSet(-215.63, 286.00, "Mondstein", itemIcon, currentMap);
+markerSet(-293.25, 99.25, "Riesenpilz", hiddenIcon, currentMap);
+markerSet(-100.75, 175.25, "Beleber", itemIcon, currentMap);
+markerSet(-141.38, 158.38, "Mondstein", hiddenIcon, currentMap);
+markerSet(-121.25, 91.88, "Gegengift", itemIcon, currentMap);
+markerSet(-252.88, 258.25, "Mini-Pilz", hiddenIcon, currentMap);
 
 var currentMap = "Felstunnel";
 markerSet(-402.63, 229.50, "Overworld", entranceIcon, currentMap);
-markerSet(-273.13, 305.88, "Repel", itemIcon, currentMap);
-markerSet(-239.63, 165.88, "Revive", itemIcon, currentMap);
-markerSet(-340.63, 243.25, "Hard Stone", itemIcon, currentMap);
-markerSet(-207.00, 164.75, "TM(RockSlide)", tmIcon, currentMap);
-markerSet(-98.50, 193.88, "Max Ether", itemIcon, currentMap);
-markerSet(-340.13, 289.38, "Pearl", itemIcon, currentMap);
+markerSet(-273.13, 305.88, "Schutz", itemIcon, currentMap);
+markerSet(-239.63, 165.88, "Beleber", itemIcon, currentMap);
+markerSet(-340.63, 243.25, "Felsbrocken", itemIcon, currentMap);
+markerSet(-207.00, 164.75, "TM(Steinhagel)", tmIcon, currentMap);
+markerSet(-98.50, 193.88, "Top-Äther", itemIcon, currentMap);
+markerSet(-340.13, 289.38, "Perle", itemIcon, currentMap);
 
 var currentMap = "Siegesstraße";
 markerSet(-386.13, 198.75, "Overworld", entranceIcon, currentMap);
 markerSet(-265.13, 350.75, "Overworld", entranceIcon, currentMap);
-markerSet(-306.25, 221.50, "Fu", hiddenIcon, currentMap);
-markerSet(-314.50, 202.88, "Rare Candy", itemIcon, currentMap);
-markerSet(-349.50, 203.75, "Great Ball", hiddenIcon, currentMap);
-markerSet(-306.13, 211.63, "TM(Aura Sphere)", tmIcon, currentMap);
-markerSet(-266.50, 258.75, "Full Restore", itemIcon, currentMap);
-markerSet(-241.75, 320.00, "TM(Hyper Beam)", tmIcon, currentMap);
-markerSet(-149.00, 311.50, "Full Restore", itemIcon, currentMap);
-markerSet(-157.38, 207.38, "TM(Overheat)", tmIcon, currentMap);
-markerSet(-238.13, 227.63, "Protector", itemIcon, currentMap);
-markerSet(-266.25, 215.25, "TM(Focus Blast)", itemIcon, currentMap);
-
+markerSet(-314.50, 202.88, "Sonderbonbon", itemIcon, currentMap);
+markerSet(-349.50, 203.75, "Superball", hiddenIcon, currentMap);
+markerSet(-306.13, 211.63, "TM(Aurasphäre)", tmIcon, currentMap);
+markerSet(-266.50, 258.75, "Top-Genesung", itemIcon, currentMap);
+markerSet(-241.75, 320.00, "TM(Hyperstrahl)", tmIcon, currentMap);
+markerSet(-149.00, 311.50, "Top-Genesung", itemIcon, currentMap);
+markerSet(-157.38, 207.38, "TM(Überhitzung)", tmIcon, currentMap);
+markerSet(-238.13, 227.63, "Schutzschild", itemIcon, currentMap);
+markerSet(-266.25, 215.25, "TM(Fokusstoß)", itemIcon, currentMap);
 
 var currentMap = "Pokémon-Turm";
 markerSet(-427.50, 289.25, "Overworld", entranceIcon, currentMap);
-markerSet(-215.50, 295.00, "Escape Rope", itemIcon, currentMap);
-markerSet(-403.50, 203.88, "Elixir", itemIcon, currentMap);
-markerSet(-423.00, 200.00, "Great Ball", itemIcon, currentMap);
-markerSet(-403.38, 188.25, "Awakening", itemIcon, currentMap);
-markerSet(-298.25, 184.75, "TinyMushroom", hiddenIcon, currentMap);
-markerSet(-322.13, 200.63, "Cleanse Tag", itemIcon, currentMap);
-markerSet(-350.13, 180.75, "Nugget", hiddenIcon, currentMap);
-markerSet(-271.00, 216.50, "X Accuracy", itemIcon, currentMap);
-markerSet(-271.13, 176.13, "Rare Candy", itemIcon, currentMap);
+markerSet(-215.50, 295.00, "Fluchtseil", itemIcon, currentMap);
+markerSet(-403.50, 203.88, "Elixier", itemIcon, currentMap);
+markerSet(-423.00, 200.00, "Superball", itemIcon, currentMap);
+markerSet(-403.38, 188.25, "Aufwecker", itemIcon, currentMap);
+markerSet(-298.25, 184.75, "Mini-Pilz", hiddenIcon, currentMap);
+markerSet(-322.13, 200.63, "Reinigungstag", itemIcon, currentMap);
+markerSet(-350.13, 180.75, "Goldene Nugget", hiddenIcon, currentMap);
+markerSet(-271.00, 216.50, "X-Treffer", itemIcon, currentMap);
+markerSet(-271.13, 176.13, "Sonderbonbon", itemIcon, currentMap);
 
 
 var currentMap = "Kraftwerk";
 markerSet(-419.75, 111.25, "Overworld", entranceIcon, currentMap);
 markerSet(-199.50, 73.75, "Overworld", entranceIcon, currentMap);
-markerSet(-326.00, 125.00, "Hyper Potion", itemIcon, currentMap);
-markerSet(-287.50, 389.75, "TM(Agility)", tmIcon, currentMap);
-markerSet(-285.75, 279.00, "Electirizer", itemIcon, currentMap);
-markerSet(-406.75, 437.50, "TM(Thunder)", tmIcon, currentMap);
-markerSet(-143.00, 430.75, "Metal Coat", itemIcon, currentMap);
-markerSet(-150.00, 358.00, "Rare Candy", itemIcon, currentMap);
-markerSet(-241.50, 302.75, "Max Elixir", hiddenIcon, currentMap);
-markerSet(-207.75, 130.25, "Thunderstone", hiddenIcon, currentMap);
-
+markerSet(-326.00, 125.00, "Hypertrank", itemIcon, currentMap);
+markerSet(-287.50, 389.75, "TM(Agilität)", tmIcon, currentMap);
+markerSet(-285.75, 279.00, "Stromisierer", itemIcon, currentMap); // Electirizer
+markerSet(-406.75, 437.50, "TM(Donner)", tmIcon, currentMap);
+markerSet(-143.00, 430.75, "Metallmantel", itemIcon, currentMap); // Metal Coat
+markerSet(-150.00, 358.00, "Sonderbonbon", itemIcon, currentMap);
+markerSet(-241.50, 302.75, "Elixier", hiddenIcon, currentMap);
+markerSet(-207.75, 130.25, "Donnerstein", hiddenIcon, currentMap);
 
 //  var currentMap =  "Silph-Co";
 
